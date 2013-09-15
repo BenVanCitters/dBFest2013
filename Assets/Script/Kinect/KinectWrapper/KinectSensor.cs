74666d76a71c8126fa932a3de8521fe7bbf998c5
Ben Van Citters <van.citters.b@gmail.com>
2013-09-14 20:34:35 -0700

    more verbose error reporting

--- Assets/Script/Kinect/KinectWrapper/KinectSensor.cs
+++ Assets/Script/Kinect/KinectWrapper/KinectSensor.cs
@@ -107,7 +107,7 @@ public class KinectSensor : MonoBehaviour, KinectInterface {
 			int hr = NativeMethods.NuiInitialize(NuiInitializeFlags.UsesDepthAndPlayerIndex | NuiInitializeFlags.UsesSkeleton | NuiInitializeFlags.UsesColor);
             if (hr != 0)
 			{
-            	throw new Exception("NuiInitialize Failed.");
+            	throw new Exception("NuiInitialize Failed. HRESULT Fail Code: " + hr);
 			}
 			
 			hr = NativeMethods.NuiSkeletonTrackingEnable(IntPtr.Zero, skeltonTrackingMode);
