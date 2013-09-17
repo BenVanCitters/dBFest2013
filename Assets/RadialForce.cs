be55d75de0c7d3c58aef4ae360f9a7f7eb56c0bf
Ben Van Citters <van.citters.b@gmail.com>
2013-09-16 20:41:06 -0700

    changes over a few days

---
+++ Assets/RadialForce.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadialForce : MonoBehaviour {
+	
+	public GameObject forceCenter = null;
+	public float Magnitude = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		recusivelyUpdateRBs(this.gameObject);
+	}
+	
+	void recusivelyUpdateRBs(GameObject go)
+	{
+		Rigidbody rb = (Rigidbody)go.GetComponent<Rigidbody>();
+		if(rb!= null)
+		{
+			Vector3 dir = go.transform.position-forceCenter.transform.position;
+			dir.Normalize();
+			dir.x = 0;
+			rb.AddForce(dir*Magnitude);
+		}
+		foreach (Transform child in go.transform)
+		{
+			recusivelyUpdateRBs(child.gameObject);
+		}
+	}
+}
---
+++ Assets/Script/kinectTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+//using Microsoft.Kinect;
+
+public class kinectTest : MonoBehaviour {
+//	private Microsoft.Kinect.KinectSensor sensor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
